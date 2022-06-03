d75dd6c901ed53d6172069b5ad1a97996d62896e
Gaboper79 <gp130579@gmail.com>
2022-06-03 14:33:50 -0300

    Estructura inicial del proyecto

---
+++ src/main/java/com/api/personas/PersonasApiAbstractApplication.java
@@ -0,0 +1,13 @@
+package com.api.personas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonasApiAbstractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonasApiAbstractApplication.class, args);
+	}
+
+}
