d6a99b8d9f6956c0dd43481efac02c01e80cdfae
Gaboper79 <gp130579@gmail.com>
2022-06-07 16:32:51 -0300

    Personalizo las excepciones

--- src/main/java/com/api/personas/exception/ApiResourceNotfoundException.java
+++ src/main/java/com/api/personas/exception/ApiResourceNotfoundException.java
@@ -1,21 +1,24 @@
 package com.api.personas.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * Excepcion Not foud 404
  * 
  * @author gabo
  *
  */
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+
 public class ApiResourceNotfoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
+	public static final String DESCRIPCION = "Recurso no encontrado";
 
-	public ApiResourceNotfoundException(String message) {
-		super(message);
+	public ApiResourceNotfoundException() {
+		super(DESCRIPCION);
+
+	}
+
+	public ApiResourceNotfoundException(String detail) {
+		super(DESCRIPCION + "-" + detail);
 
 	}
 
